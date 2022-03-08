51e72a2bc26be7100a6155d919e4f8695203ad69
jospneto <josenetopereira380@gmail.com>
2022-03-08 13:39:52 -0300

    atualizações

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,27 +2,14 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
     private LocalDate data;
 
     public Mentoria(){}
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
 
     public LocalDate getData() {
@@ -31,5 +18,14 @@ public class Mentoria {
 
     public void setData(LocalDate data) {
         this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo()+ '\'' +
+                ",descricao='" + getDescricao() + '\'' +
+                "data=" + data +
+                '}';
     }
 }
